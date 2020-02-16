b614c63477b64042030aa5b90ed8802333bcc9f5
Padmanabhan <ramprasad.ap@gmail.com>
2020-02-16 09:18:13 +0530

    First commit from local

---
+++ app/src/main/java/com/home/ram/test002/MainActivity.java
@@ -0,0 +1,22 @@
+package com.home.ram.test002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void moveToInsertActivity(View view){
+        Intent intent = new Intent(MainActivity.this,insertItem.class);
+        startActivity(intent);
+    }
+
+}
